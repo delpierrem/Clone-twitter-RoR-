ccba5c7e51b00ad127e52e87a823c42a43e3275b
Delpierre Michel <delpierre.michel@outlook.fr>
2018-08-22 10:33:14 +0200

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def accueil
+  end
+
+  def contact
+  end
+
+  def team
+  end
+
+  def services
+
+  end
+
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @baseTitle = "RoR"
+  end
+
+  test "should get accueil" do
+    get root_url
+    assert_response :success
+    assert_select "title" , "Accueil | #{@baseTitle}"
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+    assert_select "title" , "contact | #{@baseTitle}"
+  end
+
+  test "should get team" do
+    get pages_team_url
+    assert_response :success
+    assert_select "title" , "team | #{@baseTitle}"
+  end
+
+  test "should get services" do
+    get pages_services_url
+    assert_response :success
+    assert_select "title" , "services | #{@baseTitle}"
+  end
+
+end
