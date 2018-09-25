528bf0502a5da4a0bb813c10b18cbd4f56d2cec5
Delpierre Michel <delpierre.michel@outlook.fr>
2018-09-25 17:51:13 +0200

    add error validation message for users creations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #Sauvegarde confirmé <- true
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
