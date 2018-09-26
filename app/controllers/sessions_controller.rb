5a42ca65468bdb09a294554ee7d04105c2f7a3c1
Delpierre Michel <delpierre.michel@outlook.fr>
2018-09-26 16:57:13 +0200

    log out user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,10 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    # flash.now[:danger] = "Email ou mot de passe invalide !"
+    redirect_to root_path
+
   end
 
 end
