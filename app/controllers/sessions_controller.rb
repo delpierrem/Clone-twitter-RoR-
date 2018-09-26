6e18d8117c1ce0e0b43195d7cc4e9f8403b9e1dc
Delpierre Michel <delpierre.michel@outlook.fr>
2018-09-26 16:39:50 +0200

    log user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      
-
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Email ou mot de passe invalide !"
       render "new"
     end
-
   end
 
   def destroy
-
   end
 
 end
